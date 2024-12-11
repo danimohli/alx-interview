d55aef07a367bd79713397bdb7aa989890188984
danimohli <danimohlife@gmail.com>
2024-12-11 13:06:20 +0100

    prime_g

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,6 +4,8 @@ The challenge involves determining the winner of a game based
 On the strategic removal of prime numbers and their multiples
 from a set of consecutive integers.
 """
+
+
 def primes(n):
     """
     They play x rounds of the game, where n may be different for each round.
@@ -23,7 +25,6 @@ def primes(n):
 def isWinner(x, nums):
     """
     They play x rounds of the game, where n may be different for each round.
-    Assuming Maria always goes first and both players play optimally,
     determine who the winner of each game is.
     """
     if x is None or nums is None or x == 0 or nums == []:
